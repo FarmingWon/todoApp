f15e638fa9ec9a16477977dfa655f9e9be41e30e
ws0504 <thsdnjst@naver.com>
2022-03-15 08:58:04 +0900

    NavigationDrawer

--- app/src/main/java/com/ws/todomate/MainActivity.kt
+++ app/src/main/java/com/ws/todomate/MainActivity.kt
@@ -1,22 +1,45 @@
 package com.ws.todomate
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ws.todomate.login.loginActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var auth: FirebaseAuth
+    lateinit var navigationView : NavigationView
+    lateinit var drawerLayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = Firebase.auth
+
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로우어 꺼낼떄 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.navi_menu) // 홈버튼 이미지
+        supportActionBar?.setDisplayShowTitleEnabled(false)//홈타이틀 안보이게
+
+        //드로어 생성
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        //화면 이벤트 처리
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
 
         val logoutBtn = findViewById<Button>(R.id.logoutBtn)
         logoutBtn.setOnClickListener {
@@ -25,5 +48,25 @@ class MainActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            android.R.id.home->{
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu1 -> Toast.makeText(this, "메뉴1 실행", Toast.LENGTH_SHORT).show()
+            R.id.menu2 -> Toast.makeText(this, "메뉴2 실행", Toast.LENGTH_SHORT).show()
+            R.id.menu3 -> Toast.makeText(this, "메뉴3 실행", Toast.LENGTH_SHORT).show()
+        }
+        return false
     }
 }
